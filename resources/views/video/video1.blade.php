b9d982a7c3dbbb1f22c6d39abe86e8b6f5d3db93
logicainformatica18 <logicainformatica18gmail.com>
2025-07-24 16:30:49 -0500

    corregido video

--- resources/views/video/video1.blade.php
+++ resources/views/video/video1.blade.php
@@ -2,28 +2,33 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Video 1</title>
+    <title>Video sin Loop</title>
     <style>
         html, body {
             margin: 0;
             padding: 0;
             height: 100%;
             background: #000;
+            overflow: hidden;
         }
 
-        video {
+        iframe {
             position: absolute;
             top: 0;
             left: 0;
-            width: 100%;
-            height: 100%;
-            object-fit: cover;
+            width: 100vw;
+            height: 100vh;
+            border: none;
         }
     </style>
 </head>
 <body>
 
-    <video src="{{ asset('videos/video1.mp4') }}" autoplay muted loop controls></video>
+<iframe
+    src="https://aybarcorpsacc-my.sharepoint.com/personal/programador_aybarsac_com/_layouts/15/embed.aspx?UniqueId=13ebc0fd-0130-4854-9071-5403373cfcdd&embed=%7B%22af%22%3Atrue%2C%22ust%22%3Atrue%7D&referrer=StreamWebApp&referrerScenario=EmbedDialog.Create"
+    allowfullscreen
+    title="video1.mp4">
+</iframe>
 
 </body>
 </html>
